cf36abb00932f69fb5d29bcf05d6f8fd53e49ae3
James Livesey <jamesliveg@gmail.com>
2025-01-19 12:46:33 +0000

    Implement file reading for runtime

--- runtime/main.c
+++ runtime/main.c
@@ -55,11 +55,42 @@ void peek() {
 }
 
 int main(int argc, char* argv[]) {
-    vm = c32_new(code, sizeof(code));
+    if (argc < 2) {
+        fprintf(stderr, "No input file specified\n");
+
+        return 1;
+    }
+
+    FILE* fp = fopen(argv[1], "r");
+
+    if (!fp) {
+        fprintf(stderr, "Error when reading file\n");
+
+        return 1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+
+    size_t size = ftell(fp);
+    char* data = malloc(size + 1);
+
+    fseek(fp, 0, SEEK_SET);
+
+    if (fread(data, sizeof(char), size, fp) != size) {
+        fprintf(stderr, "Error reading file contents\n");
+        fclose(fp);
+
+        free(data);
+
+        return 1;
+    }
+
+    data[size] = '\0';
+
+    vm = c32_new(data, size);
 
     while (vm->running) {
         c32_step(vm);
-        // peek();
     }
 
     return 0;
